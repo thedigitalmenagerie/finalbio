9b9ff2a82f061bdcad5d053747508759c80c7b4c
Honey-Rae Swan <honeyraeswan@gmail.com>
2022-03-08 11:24:42 -0500

    removed phone it looks bad on mobile

--- src/Components/Accessible/Footer/FooterRight/FooterRight.js
+++ src/Components/Accessible/Footer/FooterRight/FooterRight.js
@@ -4,11 +4,11 @@ import {
   FooterRightContainer,
   OuterLinks,
   OLImage,
-  Button,
-  Modal,
-  ButtonTwo
+  // Button,
+  // Modal,
+  // ButtonTwo
 } from './FooterRightElements';
-import Resume from '../../PDF/PDF';
+// import Resume from '../../PDF/PDF';
 import FB from '../../../../Assets/OuterLinks/FB.png';
 import GH from '../../../../Assets/OuterLinks/GH.png';
 import IG from '../../../../Assets/OuterLinks/IG.png';
@@ -16,24 +16,24 @@ import LB from '../../../../Assets/OuterLinks/LB.png';
 import LI from '../../../../Assets/OuterLinks/LI.png';
 import S from '../../../../Assets/OuterLinks/S.png';
 import T from '../../../../Assets/OuterLinks/T.png';
-import phone from '../../../../Assets/OuterLinks/phone.png';
+// import phone from '../../../../Assets/OuterLinks/phone.png';
 import email from '../../../../Assets/OuterLinks/email.png';
-import d from '../../../../Assets/delete.png';
+// import d from '../../../../Assets/delete.png';
 
 function FooterRight() {
-  const [modalIsOpen, setIsOpen] = React.useState(false);
+  // const [modalIsOpen, setIsOpen] = React.useState(false);
 
-  function openModal() {
-    setIsOpen(true);
-  }
+  // function openModal() {
+  //   setIsOpen(true);
+  // }
 
-  function closeModal() {
-    setIsOpen(false);
-  }
+  // function closeModal() {
+  //   setIsOpen(false);
+  // }
   return (
     <FooterRightContainer className="FooterRightContainer">
         <OuterLinks className="OuterLinks">
-          <Button onClick={openModal}><OLImage src={phone} className="OLImage"/></Button>
+          {/* <Button onClick={openModal}><OLImage src={phone} className="OLImage"/></Button> */}
             <a href="https://github.com/thedigitalmenagerie" target="_blank" rel="noopener noreferrer"><OLImage src={GH} className="OLImage"/></a>
             <a href="https://www.linkedin.com/in/honeyraeswan/" target="_blank" rel="noopener noreferrer"><OLImage src={LI} className="OLImage"/></a>
             <a href="https://open.spotify.com/playlist/1eoJ8lWVrKzPBuyVHx2q55?si=81bebeca425c4898" target="_blank" rel="noopener noreferrer"><OLImage src={S} className="OLImage"/></a>
@@ -43,13 +43,13 @@ function FooterRight() {
             <a href="https://www.facebook.com/Honey-Rae-Swan-106115451890080/" target="_blank" rel="noopener noreferrer"><OLImage src={FB} className="OLImage"/></a>
             <a href="mailto:honeyraeswan@gmail.com?subject=So,%20I%20was%20looking%20at%20your%20site%20and%20..." target="_blank" rel="noopener noreferrer"><OLImage src={email} className="OLImage"/></a>
           </OuterLinks>
-          <Modal
+          {/* <Modal
             isOpen={modalIsOpen}
             className="Modal"
           >
             <Resume><OLImage src={d}/></Resume>
             <ButtonTwo onClick={closeModal}>BACK</ButtonTwo>
-          </Modal>
+          </Modal> */}
     </FooterRightContainer>
   );
 }
